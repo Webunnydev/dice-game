e86dbabf1dac56927a59dd086c2de94154127260
Yamuna Raja <webbyperso@gmail.com>
2023-07-04 14:08:29 +0530

    integerated JS in HTML
    calling the function "dice()" onclick of the button with id "roll"

--- script/dice.js
+++ script/dice.js
@@ -1,26 +1,60 @@
 function dice(){
-    document.querySelector(".dice").style.flexDirection="row";
-    var diceRange = Math.floor(Math.random()*6);
-
-    if (diceRange === 0){
-        document.querySelector("img#white").setAttribute("src", "../images/wone.png")
+    function whiteDice(){
+        document.querySelector(".dice").style.flexDirection="row";
+        document.querySelector(".dice").style.marginBottom="5rem";    
+        document.querySelector("img#white").style.paddingRight="5rem";   
+        document.querySelector("img#white").style.height="15rem";
+        document.querySelector("img#white").style.width="15rem";
+        var diceRange = Math.floor(Math.random()*6);
+    
+        if (diceRange === 0){
+            document.querySelector("img#white").setAttribute("src", "../images/wone.png")
+        }
+        else if (diceRange === 1){
+            document.querySelector("img#white").setAttribute("src", "../images/wtwo.png")   
+        }
+        else if (diceRange === 2){
+            document.querySelector("img#white").setAttribute("src", "../images/wthree.png")   
+        }
+        else if (diceRange === 3){
+            document.querySelector("img#white").setAttribute("src", "../images/wfour.png")   
+        }
+        else if (diceRange === 4){
+            document.querySelector("img#white").setAttribute("src", "../images/wfive.png")   
+        }
+        else{
+            document.querySelector("img#white").setAttribute("src", "../images/wsix.png")   
+        }
     }
-    else if (diceRange === 1){
-        document.querySelector("img#white").setAttribute("src", "../images/wtwo.png")   
+    
+    console.log(whiteDice());
+    
+    function blackDice(){
+     
+        document.querySelector("img#black").style.height="15rem";
+        document.querySelector("img#black").style.width="15rem";
+    
+        var diceRange = Math.floor(Math.random()*6);
+    
+        if (diceRange === 0){
+            document.querySelector("img#black").setAttribute("src", "../images/bone.png")
+        }
+        else if (diceRange === 1){
+            document.querySelector("img#black").setAttribute("src", "../images/btwo.png")   
+        }
+        else if (diceRange === 2){
+            document.querySelector("img#black").setAttribute("src", "../images/bthree.png")   
+        }
+        else if (diceRange === 3){
+            document.querySelector("img#black").setAttribute("src", "../images/bfour.png")   
+        }
+        else if (diceRange === 4){
+            document.querySelector("img#black").setAttribute("src", "../images/bfive.png")   
+        }
+        else{
+            document.querySelector("img#black").setAttribute("src", "../images/bsix.png")   
+        }
     }
-    else if (diceRange === 2){
-        document.querySelector("img#white").setAttribute("src", "../images/wthree.png")   
-    }
-    else if (diceRange === 3){
-        document.querySelector("img#white").setAttribute("src", "../images/wfour.png")   
-    }
-    else if (diceRange === 4){
-        document.querySelector("img#white").setAttribute("src", "../images/wfive.png")   
-    }
-    else{
-        document.querySelector("img#white").setAttribute("src", "../images/wsix.png")   
-    }
+    
+    console.log(blackDice());
 }
-
-console.log(dice())
-
